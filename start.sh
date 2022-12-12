f16ca5828931ef78daddb3c5991123c9aef96f19
bissness1 <107295935+bissness1@users.noreply.github.com>
2022-12-12 14:25:06 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ if [[ -n $RCLONE_CONFIG ]]; then
  echo "Rclone config detected"
  echo -e "$RCLONE_CONFIG" > /app/rclone.conf
 fi
+aria2 wget curl busybox unzip unrar tar python3 ffmpeg python3-pip
 chmod +x extract
 python3 -m tobrot
-
